6725faa7de7872963095670ea730b1ef56194b48
Baron King <bkkcreative16@gmail.com>
2022-11-08 20:28:33 -0600

    Added logo and list elements

--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assests/logo.svg';
+
+const HeaderStyles = styled.header`
+  display: flex;
+`;
+
+const NavList = styled.ul`
+  display: flex;
+`;
+const NavListItem = styled.li`
+  display: flex;
+  font-weight: 400;
+  font-size: 1.6rem;
+  color: #636363;
+`;
 
 function Header() {
   return (
-    <header>
-      Header Placeholder
-    </header>
+    <HeaderStyles>
+      <Link to="/">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <NavList>
+        <NavListItem>Search</NavListItem>
+        <NavListItem>How it works</NavListItem>
+        <NavListItem>About</NavListItem>
+      </NavList>
+    </HeaderStyles>
   );
 }
 
