8b9747959e59531f7ef6772a28f429ba3c1fed13
Baron King <bkkcreative16@gmail.com>
2022-11-09 17:08:34 -0600

    Fixed test cases

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from '.';
+import App from './App';
 
 test('renders Appa', () => {
   render(<MemoryRouter><App /></MemoryRouter>);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './app';
+import App from './app/App';
 
 // eslint-disable-next-line react/jsx-filename-extension
 ReactDOM.render(
