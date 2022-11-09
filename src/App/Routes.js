b26fe2ea2a6e6086f2ab77c816fbc6e01bb9f597
Baron King <bkkcreative16@gmail.com>
2022-11-08 19:30:38 -0600

    fixed useroutes error

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Home from '../Home';
 import Search from '../Search';
 
-const RouteHandler = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/search" element={<Search />} />
-  </Routes>
-);
+const RouteHandler = () => {
+  const element = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/search', element: <Search /> },
+  ]);
+
+  return element;
+};
+// <Routes>
+//   <Route path="/" element={<Home />} />
+//   <Route path="/search" element={<Search />} />
+// </Routes>
 
 export default RouteHandler;
