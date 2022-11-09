1d4c365114bd379b2454d1adf5f16af7e241092b
Baron King <bkkcreative16@gmail.com>
2022-11-09 17:26:00 -0600

    fixed eslint file

--- src/App/index.js
+++ src/App/index.js
@@ -1,1 +1,16 @@
-export { default } from './App';
+import React from 'react';
+
+// import { RouterProvider } from 'react-router-dom';
+import Layout from '../shared/components/Layout/Layout';
+import RouteHandler from './Routes';
+
+function App() {
+  return (
+    <>
+      <Layout />
+      <RouteHandler />
+    </>
+  );
+}
+
+export default App;
