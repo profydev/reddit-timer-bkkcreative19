da23c2e8bd5490110ec46f7f56d903b463a762c8
Baron King <bkkcreative16@gmail.com>
2022-11-08 20:39:45 -0600

    added links and pointed them to right locations

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import RouteHandler from './Routes';
 import BaseStyles from './BaseStyles';
 import NormalizeStyles from './NormalizeStyles';
@@ -6,12 +7,12 @@ import Header from '../shared/components/Header/Header';
 
 function App() {
   return (
-    <>
+    <Router>
       <NormalizeStyles />
       <BaseStyles />
       <Header />
       <RouteHandler />
-    </>
+    </Router>
   );
 }
 
--- src/shared/components/Header/Header.js
+++ src/shared/components/Header/Header.js
@@ -5,16 +5,21 @@ import Logo from '../../../assests/logo.svg';
 
 const HeaderStyles = styled.header`
   display: flex;
+  padding: 3.2em 8em;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 const NavList = styled.ul`
   display: flex;
+  gap: 2.5em;
 `;
 const NavListItem = styled.li`
   display: flex;
   font-weight: 400;
   font-size: 1.6rem;
   color: #636363;
+  cursor: pointer
 `;
 
 function Header() {
@@ -24,9 +29,9 @@ function Header() {
         <img src={Logo} alt="logo" />
       </Link>
       <NavList>
-        <NavListItem>Search</NavListItem>
-        <NavListItem>How it works</NavListItem>
-        <NavListItem>About</NavListItem>
+        <NavListItem><Link to="/search/javascript">Search</Link></NavListItem>
+        <NavListItem><Link to="#how-it-works">How it works</Link></NavListItem>
+        <NavListItem><Link to="#about">About</Link></NavListItem>
       </NavList>
     </HeaderStyles>
   );
