a9dfbca0e401c9c9edae05bd93064bcc0e3b14be
Baron King <bkkcreative16@gmail.com>
2022-11-08 19:57:57 -0600

    created header file and added to App

--- src/App/index.js
+++ src/App/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import RouteHandler from './Routes';
 import BaseStyles from './BaseStyles';
 import NormalizeStyles from './NormalizeStyles';
+import Header from '../shared/components/Header/Header';
 
 function App() {
   return (
     <>
       <NormalizeStyles />
       <BaseStyles />
+      <Header />
       <RouteHandler />
     </>
   );
