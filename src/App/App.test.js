61d80e44a4753acadb3ee35d955b8324ae875eda
Baron King <bkkcreative16@gmail.com>
2022-11-09 17:19:07 -0600

    Fixed test cases

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import App from './App';
+import App from '.';
 
 test('renders Appa', () => {
   render(<MemoryRouter><App /></MemoryRouter>);
