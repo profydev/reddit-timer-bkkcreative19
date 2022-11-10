2116ae06e6b50f58a1edf7b7b00035791f65a4fd
Baron King <bkkcreative16@gmail.com>
2022-11-10 13:57:43 -0600

    added input and button

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -4,6 +4,11 @@ import * as S from './Search.style';
 const Search = () => (
   <S.Container>
     <S.Title>Find the best time for a subreddit</S.Title>
+    <S.Form>
+      <span>r /</span>
+      <S.Input />
+      <S.Button>Search</S.Button>
+    </S.Form>
   </S.Container>
 );
 
--- src/pages/search/Search.style.js
+++ src/pages/search/Search.style.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import styled from 'styled-components';
+import { Button as BaseButton } from '../../shared/components/Button';
 
 export const Container = styled.div``;
 
@@ -8,4 +9,28 @@ export const Title = styled.h1`
   color: ${(props) => props.theme.color.dark};
   font-weight: 400;
   text-align: center;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 36px;
+`;
+export const Input = styled.input`
+  width: 100%;
+  max-width: 370px;
+  height: 100%;
+  border: none;
+  border: 1px solid #e6e6e6;
+  outline: none;
+  margin: 0 1rem;
+`;
+
+export const Button = styled(BaseButton)`
+  background: ${(props) => props.theme.color.orange};
+  text-transform: uppercase;
+  height: 100%;
+  display: flex;
+  align-items: center;
 `;
